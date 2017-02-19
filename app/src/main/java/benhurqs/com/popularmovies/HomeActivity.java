0a75d72db9873eabb458b0c886e6c453b834d8f1
Benhur Quintino <benhur.quintino@gmail.com>
2017-02-19 17:01:36 -0300

    first commit

---
+++ app/src/main/java/benhurqs/com/popularmovies/HomeActivity.java
@@ -0,0 +1,13 @@
+package benhurqs.com.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
