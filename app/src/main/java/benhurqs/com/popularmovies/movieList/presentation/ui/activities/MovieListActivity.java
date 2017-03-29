7093f4ff4fe01f0b58293972e7c2af55491853fc
Benhur Quintino <benhur.quintino@gmail.com>
2017-03-29 11:08:14 -0300

    created viewPager movieList

--- app/src/main/java/benhurqs/com/popularmovies/movieList/presentation/ui/activities/MovieListActivity.java
+++ app/src/main/java/benhurqs/com/popularmovies/movieList/presentation/ui/activities/MovieListActivity.java
@@ -1,27 +1,28 @@
 package benhurqs.com.popularmovies.movieList.presentation.ui.activities;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import benhurqs.com.popularmovies.R;
+import benhurqs.com.popularmovies.databinding.ActivityMovielistBinding;
 import benhurqs.com.popularmovies.movieList.domain.entities.MovieListObj;
 import benhurqs.com.popularmovies.movieList.domain.usecases.MovieListType;
 import benhurqs.com.popularmovies.movieList.presentation.presenters.MovieListPresenter;
 import benhurqs.com.popularmovies.movieList.presentation.ui.views.MovieListContract;
 
-public class HomeActivity extends AppCompatActivity implements MovieListContract.View {
+public class MovieListActivity extends AppCompatActivity implements MovieListContract.View {
 
 //    private ViewMovieDetailUseCase detailUseCase;
     private MovieListPresenter presenter;
-    private TextView txtName;
+    private ActivityMovielistBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movielist);
 
         init();
     }
@@ -51,13 +52,11 @@ public class HomeActivity extends AppCompatActivity implements MovieListContract
 
     @Override
     public void loadMovieList(MovieListObj list) {
-        txtName.setText(list.movies.get(0).title);
 
     }
 
     private void init() {
         presenter = new MovieListPresenter(this);
-        txtName = (TextView)this.findViewById(R.id.txt_name);
     }
 
     public void onClickSendPopular(View view) {
